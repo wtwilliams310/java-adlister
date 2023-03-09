92d014b1cff047ff9a8f940399142c36639d1ea1
Titus Williams <wtwilliams310@gmail.com>
2023-03-09 10:32:05 -0600

    completed servlets 1-4 and bonus

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,18 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+
+// #4 and Bonus
+@WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
-
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String hello = request.getParameter("hello");
+        System.out.println(hello);
+        response.getWriter().println("<h1>Hello, World!</h1>");
     }
 }
