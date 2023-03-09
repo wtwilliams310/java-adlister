1bb08c9ab82815083b97f629dd6454ca1b191600
Titus Williams <wtwilliams310@gmail.com>
2023-03-09 09:14:48 -0600

    warmup

--- src/main/java/WelcomeZenithServlet.java
+++ src/main/java/WelcomeZenithServlet.java
@@ -5,15 +5,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-
-// User goes to http://localhost:8080/hello in the browser and sees "Hello World"
-
-@WebServlet("/hello")
-public class HelloWorldServlet extends HttpServlet  {
+@WebServlet("/welcome")
+public class WelcomeZenithServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter out = response.getWriter();
-        out.println("<h1 style='color:red'>Some text in the browser...</h1><br><h3>This is a smaller heading</h3><a href='/login'>Login Page</a>");
-
+        out.println("<h1>Welcome, Zenith!</h1>");
     }
 }
---
+++ src/main/java/login_servlet.java
@@ -0,0 +1,15 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/login")
+public class login_servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().println("<h1>Login Page</h1>");
+    }
+}
