def7c6353f68b9f9a13bd6a15bb241a820ea5219
Titus Williams <wtwilliams310@gmail.com>
2023-03-09 11:36:40 -0600

    completed bonus 2

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -7,12 +7,37 @@ import java.io.PrintWriter;
 
 
 // #4 and Bonus
+//@WebServlet("/hello")
+//public class HelloWorldServlet extends HttpServlet {
+//    @Override
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        String name = request.getParameter("name");
+////        System.out.println(name);
+//        if (name == null) {
+//            response.getWriter().println("<h1>Hello, World!</h1>");
+//
+//        } else {
+//            response.getWriter().println("<h1>Hello " + name + "</h1>");
+//        }
+//    }
+//}
+
+
+// Bonus #2
 @WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
+
+    public static int count = 0;
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String hello = request.getParameter("hello");
-        System.out.println(hello);
-        response.getWriter().println("<h1>Hello, World!</h1>");
+        String reset = request.getParameter("reset");
+        if (reset != null){
+            count = 0;
+        }
+            response.getWriter().println(count);
+            count++;
+
+
+
     }
 }
