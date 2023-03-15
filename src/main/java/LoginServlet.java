109f13e534f988c7f6bb35a918db7f29c5c2433d
Titus Williams <wtwilliams310@gmail.com>
2023-03-15 09:49:11 -0500

    completed Session and Cookies exercises

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,20 +3,30 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+        if(request.getSession().getAttribute("user") != null){
+            response.sendRedirect("/profile");
+            return;
+        }
+        request.getRequestDispatcher("WEB-INF/login.jsp").forward(request, response);
+
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        boolean validAttempt = username.equals("admin") && password.equals("password");
+        boolean validAttempt = username.equals("Titus") && password.equals("password");
 
         if (validAttempt) {
+            HttpSession session = request.getSession();
+
+            session.setAttribute("user", username);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
--- src/main/webapp/WEB-INF/profile.jsp
+++ src/main/webapp/WEB-INF/profile.jsp
@@ -1,15 +1,18 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <html>
 <head>
-    <jsp:include page="partials/head.jsp">
+    <jsp:include page="../partials/head.jsp">
         <jsp:param name="title" value="Your Profile" />
     </jsp:include>
 </head>
 <body>
-    <jsp:include page="partials/navbar.jsp" />
+    <jsp:include page="../partials/navbar.jsp" />
 
     <div class="container">
         <h1>Viewing your profile.</h1>
+        <p>Welcome ${user}</p>
     </div>
 
 </body>
